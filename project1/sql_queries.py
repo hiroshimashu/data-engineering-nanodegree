c5dc47e54cf85591bc855ba322ba431a325839d7
hiroshimashu <hiroshimashu@gmail.com>
2021-10-12 22:37:55 +0900

    add: etl pipline

--- project1/sql_queries.py
+++ project1/sql_queries.py
@@ -8,38 +8,38 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id VARCHAR, start_time INTEGER, user_id VARCHAR, level VARCHAR, song_id VARCHAR, artist_id VARCHAR, session_id INTEGER, location VARCHAR, user_agent VARCHAR)""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL, start_time TIMESTAMP, user_id VARCHAR, level VARCHAR, song_id VARCHAR, artist_id VARCHAR, session_id INT, location TEXT, user_agent TEXT)""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id VARCHAR, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, level VARCHAR)""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id INT, first_name VARCHAR, last_name VARCHAR, gender CHAR(1), level VARCHAR)""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_title VARCHAR, title VARCHAR, artist_id VARCHAR, year INTEGER, duration NUMERIC)""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR, title VARCHAR, artist_id VARCHAR, year INT, duration FLOAT)""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR, name VARCHAR, location VARCHAR, latitude NUMERIC, longitude NUMERIC)""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR, name VARCHAR, location TEXT , latitude FLOAT , longitude FLOAT)""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time INTEGER, hour INTEGER, day INTEGER, week INTEGER, month INTEGER, year INTEGER, weekday VARCHAR)""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP, hour INT, day INT, week INT, month INT, year INT, weekday VARCHAR)""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) \
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) \
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) \
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) \
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) \
 """)
 
 # FIND SONGS
 
-song_select = ("""
-""")
+song_select = ("""SELECT songs.song_id, artists.artist_id FROM songs INNER JOIN artists ON songs.artist_id = artists.artist_id \
+WHERE songs.title=%s AND artists.name=%s AND songs.duration=%s""")
 
 # QUERY LISTS
 
