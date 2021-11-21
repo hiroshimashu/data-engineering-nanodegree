121a20a5f3d2b7624f3c3f08e54781c113aab356
hiroshimashu <hiroshimashu@gmail.com>
2021-11-21 15:43:49 +0900

    add: analytics table queires

--- project3/sql_queries.py
+++ project3/sql_queries.py
@@ -136,18 +136,45 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
+   INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+   SELECT DISTINCT
+        timestamp, 
+   FROM staging_evnets AS events INNER JOIN staging_songs AS songs
+   ON events.song = songs.title AND events.artist = songs.artist_name AND events.length = songs.duration
+   WHERE song.page = 'NextSong'
 """)
 
 user_table_insert = ("""
+   INSERT INTO users (user_id, first_name, last_name, gender, level)
+   SELECT DISTINCT userId, firstName, lastName, gender, level
+   FROM staging_events
+   WHERE page = 'NextSong' AND userId IS NOT NULL   
 """)
 
 song_table_insert = ("""
+   INSERT INTO songs (song_id, title, artist_id, year, duration)
+   SELECT DISTINCT song_id, title, artist_id, year, duration
+   FROM staging_songs
+   WHERE song_id IS NOT NULL
 """)
 
 artist_table_insert = ("""
+   INSERT INTO artists (artist_id, name, location, latitude, longitude)
+   SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+   FROM staging_songs
+   WHERE artist_id IS NOT NULL
 """)
 
 time_table_insert = ("""
+   INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+   SELECT start_time,
+        extract(hour from start_time), 
+        extract(day from start_time), 
+        extract(week from start_time), 
+        extract(month from start_time), 
+        extract(year from start_time), 
+        extract(weekday from start_time)
+   FROM songplays
 """)
 
 # QUERY LISTS
